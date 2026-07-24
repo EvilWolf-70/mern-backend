4ddac6a551935df6f647ee8adc64ba96b65eb7ee
jayaprakash74 <jayaprakash.t@elitoinnovations.com>
2026-07-24 22:56:15 +0530

    mail , reset forgot

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,8 @@
 import User from "../models/User.js";
 import jwt from 'jsonwebtoken'
+import bcrypt from "bcryptjs";
+
+import transporter from "../config/mail.js";
 // @desc Register user
 // @route POST /api/auth/register
 
@@ -42,6 +45,9 @@ export const registerUser = async (req, res) => {
 };
 
 
+// @desc Logib user
+// @route POST /api/auth/login
+
 export const loginUser = async (req, res) =>{
 
   try {
@@ -66,3 +72,95 @@ export const loginUser = async (req, res) =>{
         res.status(500).json({message:error.message})
   }
 }
+
+
+// @desc forgot-password 
+// @route POST /api/auth/forgot-password
+
+export const forgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "15m" }
+    );
+
+    const resetLink = `${process.env.CLIENT_URL}/reset-password/${token}`;
+
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: user.email,
+      subject: "Reset Your Password",
+      html: `
+        <h2>Password Reset</h2>
+
+        <p>Hello ${user.name},</p>
+
+        <p>Click the button below to reset your password.</p>
+
+        <a href="${resetLink}"
+           style="
+             background:#2563eb;
+             color:white;
+             padding:12px 20px;
+             text-decoration:none;
+             border-radius:6px;
+             display:inline-block;
+           ">
+           Reset Password
+        </a>
+
+        <p>This link expires in 15 minutes.</p>
+      `,
+    });
+
+    res.json({
+      success: true,
+      message: "Password reset link sent successfully.",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+
+export const resetPassword = async (req, res) => {
+  try {
+    const { token, password } = req.body;
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    );
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.findByIdAndUpdate(decoded.userId, {
+      password: hashedPassword,
+    });
+
+    res.json({
+      success: true,
+      message: "Password updated successfully.",
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "Invalid or expired token.",
+    });
+  }
+};
