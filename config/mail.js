799dc96412072d15fb722c00ea13f1a8c4fea08b
jayaprakash74 <jayaprakash.t@elitoinnovations.com>
2026-07-27 13:02:13 +0530

    services gmail

--- config/mail.js
+++ config/mail.js
@@ -1,10 +1,7 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  family: 4,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
