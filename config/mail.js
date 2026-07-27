38c8590e3bdcce2230ce0c33854776a4c1f03a9e
jayaprakash74 <jayaprakash.t@elitoinnovations.com>
2026-07-27 13:20:14 +0530

    services gmail

--- config/mail.js
+++ config/mail.js
@@ -1,10 +1,20 @@
 import nodemailer from "nodemailer";
 
+import dns from 'dns'
+
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  lookup(hostname, options, callback) {
+    return dns.lookup(hostname, { family: 4 }, callback);
   },
 });
 
